0e89d85d68b3337ec6187db18e4f1fb2af9b6fc7
jamesvincentsiauw-mac <vincent.vincent@bfi.co.id>
2023-11-04 23:00:59 +0700

    added problem description

--- leetcode/go/length_of_last_word.go
+++ leetcode/go/length_of_last_word.go
@@ -2,6 +2,25 @@ package main
 
 import "strings"
 
+/*
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+A word is a maximal substring consisting of non-space characters only.
+
+Example 1
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+
+Example 2
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+
+Example 3
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+*/
 func lengthOfLastWord(s string) int {
 	splittedString := strings.Split(strings.TrimSpace(s), " ")
 	return len(splittedString[len(splittedString)-1])
--- leetcode/go/merge_two_sorted_list.go
+++ leetcode/go/merge_two_sorted_list.go
@@ -5,6 +5,23 @@ type ListNode struct {
 	Next *ListNode
 }
 
+/*
+You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+Return the head of the merged linked list.
+
+Example 1
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2
+Input: list1 = [], list2 = []
+Output: []
+
+Example 3
+Input: list1 = [], list2 = [0]
+Output: [0]
+*/
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	if list1 == nil {
 		return list2
